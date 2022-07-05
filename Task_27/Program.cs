b29ec9c8840539f0c409d900f38c53c6a748f37e
Alexandr Vorobjev <vorobjev.alexandr-2017@yandex.ru>
2022-07-05 14:38:23 +0300

    sum digit in number

---
+++ Task_27/Program.cs
@@ -0,0 +1,21 @@
+﻿/* Задача 27: Напишите программу, которая принимает на вход число
+и выдаёт сумму цифр в числе.
+452 -> 11
+82 -> 10
+9012 -> 12 */
+
+Console.Write("Введите число : ");
+int number = Convert.ToInt32(Console.ReadLine());
+int sum = 0;
+int dig = number;
+int Sum(int number)
+{
+    while (number > 0)
+    {
+        sum = sum + number % 10;
+        number = number / 10;
+    }
+    return sum;
+}
+int result = Sum(number);
+Console.Write($"Сумма цифр в числе {dig} равно: {result}");
