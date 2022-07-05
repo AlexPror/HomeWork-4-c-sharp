6d6a1db7304d71ee666d81cf0e7fcbf11db8a10e
Alexandr Vorobjev <vorobjev.alexandr-2017@yandex.ru>
2022-07-05 14:39:21 +0300

    print array

---
+++ Task_29/Program.cs
@@ -0,0 +1,17 @@
+﻿/* Задача 29: Напишите программу, которая задаёт массив из 8 элементов 
+и выводит их на экран.
+1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+6, 1, 33 -> [6, 1, 33] */
+
+int[] array = new int[8];
+Random rnd = new Random();
+Console.WriteLine($"Массив из 8 элементов:");
+void PrintArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = rnd.Next(0, 10);
+        Console.Write($"{array[i]} ");
+    }
+}
+PrintArray(array);
